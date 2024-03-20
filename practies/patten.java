785483ed8c19f379ebbdaa391e0b4bf80c36ef06
Harendra Piprotar <piprotarharendra@gmail.com>
2024-03-20 23:57:37 +0530

    patten using java

---
+++ practies/patten.java
@@ -0,0 +1,20 @@
+package practies;
+
+import java.util.Scanner;
+
+import java.util.*;
+
+public class patten {
+
+    Scanner sc = new Scanner(System.in);
+  int row=sc.nextInt();
+
+    for(
+    int i = 0;i<row;i++)
+    {
+        for (int j = 0; j <= i; j++) {
+            System.out.print("* ");
+        }
+        System.out.println();
+    }
+}
